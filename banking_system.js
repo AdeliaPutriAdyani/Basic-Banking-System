ea2753b4d76e1a91d9b79979bc61f444bfc6c84e
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-09-08 14:57:35 +0700

    Basic-Banking-System

--- banking_system.js
+++ banking_system.js
@@ -6,7 +6,7 @@ class BankAccount {
     }
 
     saldoAwal() {
-        let saldoAwalMessage = "Saldo Awal = " + this.saldo;
+        let saldoAwalMessage = "Saldo Awal = Rp" + this.saldo;
         alert(saldoAwalMessage);
         console.log(saldoAwalMessage);
     }
@@ -16,7 +16,7 @@ class BankAccount {
             setTimeout(() => {
                 if (amount > 0) {
                     this.saldo += amount;
-                    resolve(`Deposit dengan Rp${amount} Berhasil diperbarui. \nSaldo Anda : ${this.saldo}`)
+                    resolve(`Deposit dengan Rp${amount} Berhasil diperbarui. \nSaldo Anda : Rp${this.saldo}`)
                 } else {
                     reject(new Error ('Deposit harus lebih besar dari 0'));
                 }
@@ -29,7 +29,7 @@ class BankAccount {
             setTimeout(() => {
                 if (amount > 0 && amount <= this.saldo) {
                     this.saldo -= amount;
-                    resolve(`Penarikan sejumlah ${amount} berhasil. \nSaldo baru: ${this.saldo}`)
+                    resolve(`Penarikan sejumlah ${amount} berhasil. \nSaldo baru: Rp${this.saldo}`)
                 } else if (amount > this.saldo) {
                     reject(new Error ('Saldo Anda tidak mencukupi untuk penarikan !'));
                 } else {
@@ -47,6 +47,9 @@ const aksi = prompt("Pilih aksi yang ingin dilakukan:\n1. Tambah Saldo/Deposit\n
 
 if (aksi === "1") {
     let amount = parseFloat(prompt("Masukkan jumlah saldo/deposit: "));
+    let pesan = `PROSES SEDANG BERLANGSUNG\nMemasukan Deposit Rp${amount}`
+    console.log(pesan)
+    alert(pesan)
     account.deposit(amount)
         .then(resolve => {
             alert(resolve);
@@ -58,6 +61,9 @@ if (aksi === "1") {
         });
 } else if (aksi === "2") {
     let amount = parseFloat(prompt("Masukkan jumlah penarikan/withdraw: "));
+    let pesan = `PROSES SEDANG BERLANGSUNG\nMenarik saldo Rp${amount}`
+    console.log(pesan)
+    alert(pesan)
     account.withdraw(amount)
         .then(resolve => {
             alert(resolve);
